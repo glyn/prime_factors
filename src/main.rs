bae22afdeb6025b58e81128ee6ff8f84414cca67
Glyn Normington <glyn.normington@gmail.com>
2021-02-24 18:14:27 +0000

    Make sieve an iterator
    This optimises the case where not all values in the sieve are needed to
    factorise the input number.

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ fn prime_factors(n: i64) -> String {
     res
 }
 
-fn p_factors(n: i64) -> HashMap<i64, i64> {
-    let fac = sieve((n as f64).sqrt() as i64);
+fn p_factors<'a>(n: i64) -> HashMap<i64, i64> {
+    let fac = new_sieve((n as f64).sqrt() as i64);
     let mut factors = HashMap::new();
     let mut rem = n;
-    for i in &fac {
+    for i in fac {
         while rem % i == 0 {
             rem = rem / i;
-            *factors.entry(*i).or_insert(0) += 1;
+            *factors.entry(i).or_insert(0) += 1;
         }
         if rem == 1 {
             break;
@@ -39,38 +39,65 @@ fn p_factors(n: i64) -> HashMap<i64, i64> {
     factors
 }
 
-// sieve returns all the prime less than or equal to the input value.
+struct Sieve {
+    s: Vec<bool>,
+    p: usize,
+    sqrt_n: usize,
+}
+
+// new_sieve returns an iterator of all the prime less than or equal to the input value.
 // See: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-fn sieve(n: i64) -> Vec<i64> {
-    let mut s: Vec<bool> = Vec::with_capacity(n as usize); // after sieving, true at index i means i+1 is prime
-    s.push(false); // 1 is not prime
+fn new_sieve(n: i64) -> Sieve {
+    let mut v = Sieve {
+        s: Vec::with_capacity(n as usize),
+        p: 2,
+        sqrt_n: (n as f64).sqrt() as usize,
+    };
+    v.s.push(false); // 1 is not prime
     for _ in 1..n {
-        s.push(true);
+        v.s.push(true);
     }
-    let mut i = 2;
-    while i as f64 <= (n as f64).sqrt() {
-        if s[i as usize - 1] {
-            // if p is prime
-            let mut j = i * i;
-            while j <= n {
-                s[j as usize - 1] = false;
-                j += i;
+    v
+}
+
+impl Iterator for Sieve {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.p <= self.sqrt_n {
+            if self.s[self.p - 1] {
+                // self.p is prime
+                let mut j = self.p * self.p;
+                while j <= self.s.len() {
+                    self.s[j - 1] = false;
+                    j += self.p;
+                }
+                let r = Some(self.p as i64);
+                self.p += 1;
+                return r;
             }
+            self.p += 1;
         }
-        i += 1
-    }
-    let mut primes = vec![];
-    for i in 0..s.len() {
-        if s[i] {
-            primes.push((i + 1) as i64)
+        while self.p <= self.s.len() {
+            if self.s[self.p - 1] {
+                let r = Some(self.p as i64);
+                self.p += 1;
+                return r;
+            }
+            self.p += 1;
         }
+        None
     }
-    primes
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn sieve() {
+        assert_eq!(new_sieve(5).collect::<Vec<i64>>(), vec![2, 3, 5]);
+    }
 
     fn testing(n: i64, exp: &str) -> () {
         assert_eq!(&prime_factors(n), exp)
